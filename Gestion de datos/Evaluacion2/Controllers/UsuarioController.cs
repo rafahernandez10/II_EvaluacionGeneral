0165d277ca84b46e00a55285f2f7d8dbf0ebb190
anjemo2903 <molinajesus2003@gmail.com>
2024-08-19 20:02:54 -0600

    Creacion de Models

---
+++ Gestion de datos/Evaluacion2/Controllers/UsuarioController.cs
@@ -0,0 +1,10 @@
+﻿using Evaluacion2.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace Evaluacion2.Controllers
+{
+    public class UsuarioController
+    {
+        
+
+    }
+}
--- Gestion de datos/Evaluacion2/obj/Debug/net8.0/Evaluacion2.AssemblyInfo.cs
+++ Gestion de datos/Evaluacion2/obj/Debug/net8.0/Evaluacion2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Evaluacion2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+642cc3f093da87079b5900bdcc20fafdb2cfdb5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e67294312be757aeba7c7828f1bd616c26f07cd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Evaluacion2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Evaluacion2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
