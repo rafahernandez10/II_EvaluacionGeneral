c80155067c45aa3145d710c18933b62f089f3fe2
jmm7gameover <jmm7gameover@gmail.com>
2024-08-19 20:35:12 -0600

    sisoi

--- Gestion de datos/Evaluacion2/obj/Debug/net8.0/Evaluacion2.AssemblyInfo.cs
+++ Gestion de datos/Evaluacion2/obj/Debug/net8.0/Evaluacion2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Evaluacion2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e67294312be757aeba7c7828f1bd616c26f07cd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d14cc65b7334463122e6bf15eb9ea7ae78f53a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Evaluacion2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Evaluacion2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
