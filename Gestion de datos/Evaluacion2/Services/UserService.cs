bf52602e0ede0ede7ba8fbd655e50a94ec18bb59
jmm7gameover <jmm7gameover@gmail.com>
2024-08-21 15:43:09 -0600

    agregando servicios

---
+++ Gestion de datos/Evaluacion2/Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using Evaluacion2.Models;
+using Evaluacion2.Repositories;
+
+namespace Evaluacion2.Services
+{
+    public class UserService
+    {
+        private readonly IUsuarioRepository _userRepository;
+
+        public UserService(IUsuarioRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public IEnumerable<UsuarioModel> GetAll()
+        {
+            return _userRepository.GetAll();
+        }
+
+        public UsuarioModel GetById(int id)
+        {
+            return _userRepository.GetById(id);
+        }
+
+        public void Add(UsuarioModel user)
+        {
+            _userRepository.Add(user);
+        }
+
+        public void Update(UsuarioModel user)
+        {
+            _userRepository.Update(user);
+        }
+
+        public void Delete(int id)
+        {
+            _userRepository.Delete(id);
+        }
+
+    }
+}
--- Gestion de datos/Evaluacion2/obj/Debug/net8.0/Evaluacion2.AssemblyInfo.cs
+++ Gestion de datos/Evaluacion2/obj/Debug/net8.0/Evaluacion2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Evaluacion2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cc7e727f9f8f8873ce4f71127d23216ede264fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1863811600753d818115730cb68995cb6faaea54")]
 [assembly: System.Reflection.AssemblyProductAttribute("Evaluacion2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Evaluacion2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
